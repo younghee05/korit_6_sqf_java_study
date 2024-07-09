c57e3532b294b770bb13767374a71c8a9bbea52b
younghee05 <k1437013@gmail.com>
2024-07-09 13:58:05 +0900

    DB 설정

---
+++ dvd/src/main/java/com/study/dvd/db/DBConfig.java
@@ -0,0 +1,7 @@
+package com.study.dvd.db;
+
+public interface DBConfig {
+    String URL = "jdbc:mysql://korea-it.cng2osy0onis.ap-northeast-2.rds.amazonaws.com:3306/dvd_db";
+    String USERNAME = "admin";
+    String PASSWORD = "1q2w3e4r!!";
+}
---
+++ dvd/src/main/java/com/study/dvd/views/DVDListView.java
@@ -0,0 +1,17 @@
+package com.study.dvd.views;
+
+import java.util.Scanner;
+
+public class DVDListView {
+    public static void show() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("[ DVD 추가 화면 ]");
+        System.out.print("등록번호 입력 >>> ");
+        System.out.print("제목 입력 >>> ");
+        System.out.print("제작자 입력 >>> ");
+        System.out.print("발행사 입력 >>> ");
+        System.out.print("발행년도 입력 >>> ");
+
+    }
+}
